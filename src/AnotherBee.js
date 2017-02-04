eb5d45b351fca4c2bbde683c8a76f1111fa8a1a8
Cal Holloway <calvin.c.holloway@gmail.com>
2017-02-04 12:14:04 -0330

    made public

---
+++ src/AnotherBee.js
@@ -0,0 +1,16 @@
+import Bee from './Bee'
+
+class AnotherBee {
+  constructor() {
+    super();
+
+    this.age = 15;
+    this.job = 'test things'
+  }
+
+  someMethod() {
+    console.log('this is an example file')
+  }
+}
+
+export default AnotherBee
